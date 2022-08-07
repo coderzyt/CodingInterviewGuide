dbee2eb0c66dec49a234f47151bafbf79d8af702
zyt528 <zyt528@outlook.com>
2022-08-07 16:00:05 +0800

    optimize

---
+++ src/main/java/algorithm/sort/quick/Quick3way.java
@@ -0,0 +1,25 @@
+package algorithm.sort.quick;
+
+import algorithm.sort.Example;
+
+public class Quick3way {
+    public static void sort(Comparable[] arr, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int lt = lo, i = lo + 1, gt = hi;
+        Comparable v = arr[lo];
+        while (i <= gt) {
+            int cmp = arr[i].compareTo(v);
+            if (cmp < 0) {
+                Example.exch(arr, lt++, i++);
+            } else if (cmp > 0) {
+                Example.exch(arr, i, gt--);
+            } else {
+                i++;
+            }
+        }
+        sort(arr, lo, lt - 1);
+        sort(arr, gt + 1, hi);
+    }
+}
