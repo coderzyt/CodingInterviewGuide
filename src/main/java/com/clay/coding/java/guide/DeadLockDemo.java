ab7af5fa71304acea3d60df6f9261749b8a4788c
zyt528 <zyt528@outlook.com>
2022-08-11 01:30:30 +0800

    add deadlockdemo

---
+++ src/main/java/com/clay/coding/java/guide/DeadLockDemo.java
@@ -0,0 +1,43 @@
+package com.clay.coding.java.guide;
+
+/**
+ * @author coderclay
+ * 线程死锁描述的是这样一种情况：多个线程同时阻塞，它们中的一个或者全部都在等待某个资源被释放。
+ * 由于线程被无限期地阻塞，因此程序不可能正常终止。
+ */
+public class DeadLockDemo {
+    private static Object resource1 = new Object();
+    private static Object resource2 = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println(Thread.currentThread() + "get resource1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource2");
+                synchronized (resource2) {
+                    System.out.println(Thread.currentThread() + "get resource2");
+                }
+            }
+        }, "线程 1").start();
+
+        new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println(Thread.currentThread() + "get resource2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource1");
+                synchronized (resource1) {
+                    System.out.println(Thread.currentThread() + "get resource1");
+                }
+            }
+        }, "线程 2").start();
+    }
+}
