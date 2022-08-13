60a871ead9cd6f444fa856c2f6c5f2413f1a1270
zyt528 <zyt528@outlook.com>
2022-08-14 04:27:05 +0800

    optimize

---
+++ src/main/java/com/clay/coding/java/guide/jvm/GcTest1.java
@@ -0,0 +1,12 @@
+package com.clay.coding.java.guide.jvm;
+
+/**
+ * @author coderclay
+ */
+public class GcTest1 {
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2;
+        allocation1 = new byte[30900 * 1024];
+        allocation2 = new byte[900*1024];
+    }
+}
