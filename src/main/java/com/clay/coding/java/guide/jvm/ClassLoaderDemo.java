87a727669c3233ae629e5ae4bbfeedab6779ee36
zyt528 <zyt528@outlook.com>
2022-08-16 21:35:08 +0800

    optimize

---
+++ src/main/java/com/clay/coding/java/guide/jvm/ClassLoaderDemo.java
@@ -0,0 +1,13 @@
+package com.clay.coding.java.guide.jvm;
+
+/**
+ * @author coderclay
+ */
+public class ClassLoaderDemo {
+
+    public static void main(String[] args) {
+        System.out.println("ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader());
+        System.out.println("The Parent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent());
+        System.out.println("The GrandParent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent().getParent());
+    }
+}
