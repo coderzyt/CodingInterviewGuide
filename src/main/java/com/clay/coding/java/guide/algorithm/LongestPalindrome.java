08c2c077046f8f9fea745c6eddbc369ccc17dee3
zyt528 <zyt528@outlook.com>
2022-08-22 09:50:30 +0800

    add algorithm

---
+++ src/main/java/com/clay/coding/java/guide/algorithm/LongestPalindrome.java
@@ -0,0 +1,59 @@
+package com.clay.coding.java.guide.algorithm;
+
+import java.util.HashSet;
+
+/**
+ * @author coderclay
+ */
+public class LongestPalindrome {
+
+    public static int longestPalindrome(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        // 用于存放字符
+        HashSet<Character> hashSet = new HashSet<>();
+        char[] chars = s.toCharArray();
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (!hashSet.contains(chars[i])) {
+                hashSet.add(chars[i]);
+            } else {
+                hashSet.remove(chars[i]);
+                count++;
+            }
+        }
+        return hashSet.isEmpty() ? count * 2 : count * 2 + 1;
+    }
+
+    public static int longestPalindrome1(String s) {
+        int[] arr = new int[128];
+        for (char c : s.toCharArray()) {
+            arr[c]++;
+        }
+        int count = 0;
+        for (int v : arr) {
+            count += (v % 2);
+        }
+        return count == 0 ? s.length() : (s.length() - count + 1);
+    }
+
+    public static int longestPalindrome2(String s) {
+        int[] arr = new int[128];
+        for (char c : s.toCharArray()) {
+            arr[c]++;
+        }
+        int count = 0;
+        for (int v : arr) {
+            count += v / 2 * 2;
+            if (v % 2 == 1 && count % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LongestPalindrome.longestPalindrome("abccccdd"));
+    }
+}
---
+++ src/main/java/com/clay/coding/java/guide/algorithm/Solution.java
@@ -0,0 +1,40 @@
+package com.clay.coding.java.guide.algorithm;
+
+/**
+ * @author coderclay
+ */
+public class Solution {
+    /**
+     * 把 We Are Happy 替换成 We%20Are%20Happy
+     */
+    public static String replaceSpace(String target) {
+        int length = target.length();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            char ch = target.charAt(i);
+            if (ch == ' ') {
+                stringBuilder.append("%20");
+            } else {
+                stringBuilder.append(ch);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String replaceSpace2(String target) {
+        return target.replaceAll("\\s", "%20");
+    }
+
+    public static String replaceSpace3(String target) {
+        return target.replace(" ", "%20");
+    }
+
+    public static void main(String[] args) {
+        String result = replaceSpace("We Are Happy");
+        String result2 = replaceSpace2("We Are Happy");
+        String result3 = replaceSpace3("We Are Happy");
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+}
