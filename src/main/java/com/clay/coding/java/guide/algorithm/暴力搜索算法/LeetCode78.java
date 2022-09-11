29112f46e09c1dc3b059637c410e27501fde9252
clay <zyt528@outlook.com>
2022-09-12 03:05:01 +0800

    optimize

--- src/main/java/com/clay/coding/java/guide/algorithm/暴力搜索算法/LeetCode78.java
+++ src/main/java/com/clay/coding/java/guide/algorithm/暴力搜索算法/LeetCode78.java
@@ -1,5 +1,6 @@
 package com.clay.coding.java.guide.algorithm.暴力搜索算法;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -8,11 +9,22 @@ import java.util.List;
  */
 public class LeetCode78 {
 
-    public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> res = new LinkedList<>();
 
+    LinkedList<Integer> track = new LinkedList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(nums, 0);
+        return res;
     }
 
-    void backtrack() {
-
+    void backtrack(int[] nums, int start) {
+        // 前序位置,每个节点的值都是一个子集
+        res.add(new LinkedList<>(track));
+        for (int i = start; i < nums.length; i++) {
+            track.addLast(nums[i]);
+            backtrack(nums, i + 1);
+            track.removeLast();
+        }
     }
 }
