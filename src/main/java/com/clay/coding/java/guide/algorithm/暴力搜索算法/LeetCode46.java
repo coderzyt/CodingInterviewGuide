9694e5e04961e0d329fcf2b9d71bd42439b8d075
zyt528 <zyt528@outlook.com>
2022-09-12 16:53:49 +0800

    windows and mac format

--- src/main/java/com/clay/coding/java/guide/algorithm/暴力搜索算法/LeetCode46.java
+++ src/main/java/com/clay/coding/java/guide/algorithm/暴力搜索算法/LeetCode46.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.clay.coding.java.guide.algorithm.暴力搜索算法;
 
 import java.util.LinkedList;
@@ -40,3 +41,47 @@ public class LeetCode46 {
         }
     }
 }
+=======
+package com.clay.coding.java.guide.algorithm.暴力搜索算法;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author coderclay
+ * <a href="https://leetcode.cn/problems/permutations/">...</a>
+ */
+public class LeetCode46 {
+    List<List<Integer>> res = new LinkedList<>();
+
+    List<List<Integer>> permute(int[] nums) {
+        // 记录路径
+        LinkedList<Integer> track = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, track, used);
+        return res;
+    }
+
+    void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        // 触发角色条件
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 排除不合法的选择
+            if (used[i]) {
+                continue;
+            }
+            // 做选择
+            track.add(nums[i]);
+            used[i] = true;
+            // 进入下一层决策树
+            backtrack(nums, track, used);
+            // 取消选择
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
+>>>>>>> Stashed changes
--- src/main/java/com/clay/coding/java/guide/algorithm/暴力搜索算法/LeetCode78.java
+++ src/main/java/com/clay/coding/java/guide/algorithm/暴力搜索算法/LeetCode78.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.clay.coding.java.guide.algorithm.暴力搜索算法;
 
 import java.util.LinkedList;
@@ -34,3 +35,35 @@ public class LeetCode78 {
         leetCode78.subsets(nums);
     }
 }
+=======
+package com.clay.coding.java.guide.algorithm.暴力搜索算法;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author coderclay
+ * <a href="https://leetcode.cn/problems/subsets/">...</a>
+ */
+public class LeetCode78 {
+
+    List<List<Integer>> res = new LinkedList<>();
+
+    LinkedList<Integer> track = new LinkedList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+
+    void backtrack(int[] nums, int start) {
+        // 前序位置,每个节点的值都是一个子集
+        res.add(new LinkedList<>(track));
+        for (int i = start; i < nums.length; i++) {
+            track.addLast(nums[i]);
+            backtrack(nums, i + 1);
+            track.removeLast();
+        }
+    }
+}
+>>>>>>> Stashed changes
